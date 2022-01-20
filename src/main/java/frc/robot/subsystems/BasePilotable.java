1310f705c62d92f44c98e30336993d2ac3247495
jayson-poirier <jayson.poirier2019@gmail.com>
2022-01-19 21:32:42 -0500

    Créations de méthodes Base Pilotable

--- src/main/java/frc/robot/subsystems/BasePilotable.java
+++ src/main/java/frc/robot/subsystems/BasePilotable.java
@@ -2,9 +2,11 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.SubSystem;
+package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+import com.revrobotics.CANSparkMax.IdleMode;
 
 import edu.wpi.first.wpilibj.ADXRS450_Gyro;
 import edu.wpi.first.wpilibj.Encoder;
@@ -16,18 +18,18 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class BasePilotable extends SubsystemBase {
 // Changer les numéros de moteurs
-private WPI_TalonFX moteurAvantGauche = new WPI_TalonFX(1);
-private WPI_TalonFX moteurDerriereGauche = new WPI_TalonFX(2);
-private WPI_TalonFX moteurAvantDroit = new WPI_TalonFX(3);
-private WPI_TalonFX moteurDerriereDroit = new WPI_TalonFX(4);
+private WPI_TalonFX moteurAvantG = new WPI_TalonFX(1);
+private WPI_TalonFX moteurArriereG = new WPI_TalonFX(2);
+private WPI_TalonFX moteurAvantD = new WPI_TalonFX(3);
+private WPI_TalonFX moteurArriereD = new WPI_TalonFX(4);
 
-private MotorControllerGroup moteursGauches = new MotorControllerGroup(moteurAvantGauche, moteurDerriereGauche);
-private MotorControllerGroup moteursDroits = new MotorControllerGroup(moteurAvantDroit, moteurDerriereDroit);
+private MotorControllerGroup moteursG = new MotorControllerGroup(moteurAvantG, moteurArriereG);
+private MotorControllerGroup moteursD = new MotorControllerGroup(moteurAvantD, moteurArriereD);
 
-private DifferentialDrive drive = new DifferentialDrive(moteursGauches, moteursDroits);
+private DifferentialDrive drive = new DifferentialDrive(moteursG, moteursD);
 
-private Encoder encodeurGauche = new Encoder(0, 1,false);
-private Encoder encodeurDroit = new Encoder(2, 3,true);
+private Encoder encodeurG = new Encoder(0, 1,false);
+private Encoder encodeurD = new Encoder(2, 3,true);
 
 private ADXRS450_Gyro gyro = new ADXRS450_Gyro();
   /** Creates a new BasePilotable. */
@@ -37,14 +39,41 @@ private ADXRS450_Gyro gyro = new ADXRS450_Gyro();
   public void periodic() {
     // This method will be called once per scheduler run
   }
-  public void conduire(double vx, double vz){
-drive.arcadeDrive(vx, vz);
 
+  public void conduire(double vx, double vz){
+    drive.arcadeDrive(vx, vz);
 
   }
+
   public void autoConduire(double voltGauche, double voltDroit){
-    moteursGauches.setVoltage(voltGauche);
-    moteursDroits.setVoltage(voltDroit);
+    moteursG.setVoltage(voltGauche);
+    moteursD.setVoltage(voltDroit);
     drive.feed();
-      }
+
+  }
+
+  public void stop(){
+    drive.arcadeDrive(0, 0);
+
+  }
+  public void setRamp(double ramp){
+    moteurAvantG.configOpenloopRamp(ramp);
+    moteurArriereG.configOpenloopRamp(ramp);
+    moteurAvantD.configOpenloopRamp(ramp);
+    moteurArriereD.configOpenloopRamp(ramp);
+    
+  }
+
+  public void setNeutralMode(NeutralMode mode){
+    moteurAvantG.setNeutralMode(mode);
+    moteurArriereG.setNeutralMode(mode);
+    moteurAvantD.setNeutralMode(mode);
+    moteurArriereD.setNeutralMode(mode);
+
+  }
+
+  public void getPositionG(){
+
+
+  }  
 }
