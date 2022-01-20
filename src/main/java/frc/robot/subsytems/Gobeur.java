7e332e013233c0b23b985b8627a49de7dfd34763
Pierre-André <royp1@csdps.qc.ca>
2022-01-19 19:54:16 -0500

    Sous Système Gobeur Presque Terminer

--- src/main/java/frc/robot/subsytems/Gobeur.java
+++ src/main/java/frc/robot/subsytems/Gobeur.java
@@ -4,14 +4,44 @@
 
 package frc.robot.subsytems;
 
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
+import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Gobeur extends SubsystemBase {
-  /** Creates a new Gobeur. */
-  public Gobeur() {}
+
+  private DoubleSolenoid tiroir = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, 1, 0); //a determiner
+
+  private  
+
+  public Gobeur() {
+    
+  }
 
   @Override
   public void periodic() {
-    // This method will be called once per scheduler run
+    @Override
+    public void moteurStop() {
+      rouleau.set(0.0);
+    }
+
+    public void moteurGobe() {
+      rouleau.set(1.0);
+    }
+
+    public void moteurPanic() {
+      rouleau.set(-1.0);
+    }
+
+    public void tiroirIn() {
+      tiroir.set(Value.kReverse);
+    }
+
+    public void tiroirOut() {
+      tiroir.set(Value.kForward);
+    }
+
   }
 }
