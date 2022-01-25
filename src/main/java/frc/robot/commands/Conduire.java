01546a408adc3a2fd7c30bd08fddfab74ed14d95
Huskies Prog <huskies5439@polych.ca>
2022-01-25 13:17:55 -0500

    Création transmission

--- src/main/java/frc/robot/commands/Conduire.java
+++ src/main/java/frc/robot/commands/Conduire.java
@@ -6,6 +6,7 @@ package frc.robot.commands;
 
 import java.io.FilenameFilter;
 
+import edu.wpi.first.wpilibj.RobotState;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.BasePilotable;
 
@@ -13,7 +14,19 @@ public class Conduire extends CommandBase {
   BasePilotable basePilotable;
   double avancer;
   double tourner;
+  //Déclare et permet l'envoie des states dans le SmartDashboard.
+  /*private enum State {
+    LOW(0), HIGH(1), AUTO(2);
 
+    private int stateValue;
+
+    State(int stateValue) {
+      this.stateValue = stateValue;
+    }*/
+
+  //}
+
+  //private State state = State.AUTO;//Initialise la state en Auto.
   /** Creates a new Conduire. */
   public Conduire(double avancer, double tourner, BasePilotable basePilotable) {
     this.basePilotable = basePilotable;
@@ -31,6 +44,24 @@ public class Conduire extends CommandBase {
 
     basePilotable.conduire(avancer, tourner);
     
+    /*if (RobotState.isAutonomous()){
+      state= State.AUTO; //Pas nécéssaire en compé, mais sert a retourner en Autonomous.
+     } 
+   else if (Math.abs(basePilotable.getVitesse()) > 2 && state == State.LOW) { 
+     basePilotable.hauteVitesse(); 
+     state =State.HIGH; //Vérifier que le Robot est en basseVitesse pour passer en hauteVitesse.
+     } 
+   else if (Math.abs(basePilotable.getVitesse()) < 1.2 && state == State.HIGH) {
+     basePilotable.basseVitesse(); 
+     state = State.LOW; //Vérifier que le Robot est en hauteVitesse pour passer en basseVitesse.
+     } 
+   else if (state == State.AUTO) {
+     basePilotable.basseVitesse(); //Force la basseVitesse en Autonomous.
+     if (!RobotState.isAutonomous()) { 
+       state = State.LOW; //Après Autonomous passe dans la StateMachine principal.
+       } 
+   }*/
+
   }
 
   // Called once the command ends or is interrupted.
