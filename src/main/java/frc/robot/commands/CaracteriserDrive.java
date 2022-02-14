02bda0646fd657a84931e90b8d3371d69ca74b49
Huskies Prog <57733100+HuskiesProg@users.noreply.github.com>
2022-02-14 12:26:30 -0500

    CaractériserDrive et Lanceur, LancerSimple

--- src/main/java/frc/robot/commands/CaracteriserDrive.java
+++ src/main/java/frc/robot/commands/CaracteriserDrive.java
@@ -1,30 +1,32 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot.commands;
 
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Lanceur;
 
 public class CaracteriserDrive extends CommandBase {
-  /** Creates a new CaracteriserDrive. */
-  public CaracteriserDrive() {
-    // Use addRequirements() here to declare subsystem dependencies.
-  }
+  Lanceur lanceur;
 
-  // Called when the command is initially scheduled.
+  public CaracteriserDrive(Lanceur lanceur) {
+    this.lanceur = lanceur;
+    addRequirements(lanceur);
+
+  }
   @Override
   public void initialize() {}
 
-  // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+    lanceur.setVoltage(lanceur.getVoltageShuffleboard());
+  }
 
-  // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    lanceur.stop();
+  }
 
-  // Returns true when the command should end.
   @Override
   public boolean isFinished() {
     return false;
