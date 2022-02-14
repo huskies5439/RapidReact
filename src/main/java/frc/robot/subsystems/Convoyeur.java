23f44441f20773095896a927c899ffff5c2fe458
Huskies Prog <57733100+HuskiesProg@users.noreply.github.com>
2022-02-14 13:19:42 -0500

    Convoyeur

---
+++ src/main/java/frc/robot/subsystems/Convoyeur.java
@@ -0,0 +1,24 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Convoyeur extends SubsystemBase {
+  private WPI_TalonSRX moteurConvoyeur = new WPI_TalonSRX(15);
+  
+  public Convoyeur() {}
+
+  @Override
+  public void periodic() {
+  
+  }
+
+  public void fournir() {
+  
+  }
+}
