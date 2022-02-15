02fd3123eca09242e9244d9e90092132b581778c
Huskies Prog <huskies5439@polych.ca>
2022-02-15 13:15:31 -0500

    Update EncodeurM.java

--- src/main/java/frc/robot/commands/EncodeurM.java
+++ src/main/java/frc/robot/commands/EncodeurM.java
@@ -5,6 +5,9 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.BasePilotable;
+
+
 
 public class EncodeurM extends CommandBase {
   /** Creates a new EncodeurM. */
