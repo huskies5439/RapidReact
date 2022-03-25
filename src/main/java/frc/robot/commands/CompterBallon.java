18be48e82e1ea480888ad97cc261442a95098b6f
Huskies Prog <57733100+HuskiesProg@users.noreply.github.com>
2022-03-25 13:12:15 -0400

    fonction compter ballons

--- src/main/java/frc/robot/commands/CompterBallon.java
+++ src/main/java/frc/robot/commands/CompterBallon.java
@@ -1,29 +1,51 @@
 package frc.robot.commands;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Constants;
 import frc.robot.subsystems.Convoyeur;
 
 public class CompterBallon extends CommandBase {
   Convoyeur convoyeur;
+  int nombreBallons;
+  boolean etatActuel;
+  boolean etatPasse;
+  int limiteBallons;
 
-  public CompterBallon() {}
-
-  @Override
-  public void initialize() {}
-
-  @Override
-  public void execute() {
-    if(convoyeur.capteurHaut()) {
-      Constants.ballons +
-    }
+  public CompterBallon(int limite_ballons, Convoyeur convoyeur) {
+    this.convoyeur = convoyeur;
+    this.limiteBallons = limite_ballons;
   }
 
   @Override
-  public void end(boolean interrupted) {}
+  public void initialize() {
+    etatActuel = false;
+    etatPasse = false;
+  }
+
+  @Override
+  public void execute() {
+    etatActuel = convoyeur.capteurHaut();
+
+    if (etatActuel != etatPasse) {
+      if (etatActuel) {
+        nombreBallons++;
+      }
+      SmartDashboard.putNumber("nombre de ballon", nombreBallons);
+
+      etatPasse = etatActuel;
+
+    }
+
+  }
+
+  @Override
+  public void end(boolean interrupted) {
+  }
 
   @Override
   public boolean isFinished() {
-    return false;
+    return nombreBallons >= limiteBallons;
+
   }
 }
