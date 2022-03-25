f095ba29b0a370146b0ba3a07493a77ee8679be7
Pierre-André <royp1@csdps.qc.ca>
2022-03-25 19:51:02 -0400

    Ligne 42 et Ligne 20

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -3,22 +3,23 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.XboxController.Button;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.StartEndCommand;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
-import frc.robot.commands.AutoGrimper;
 import frc.robot.commands.Conduire;
-import frc.robot.commands.ConvoyerFancy;
+import frc.robot.commands.ConvoyerSimple;
 import frc.robot.commands.Gober;
 import frc.robot.commands.Grimper;
 import frc.robot.commands.LancerFancy;
 import frc.robot.commands.LancerSimple;
 import frc.robot.commands.TournerAuto;
 import frc.robot.commands.TournerLimelight;
-import frc.robot.commands.ViserLancer;
 import frc.robot.commands.caracteriser.CaracteriserDrive;
 import frc.robot.commands.caracteriser.CaracteriserLanceur;
 import frc.robot.subsystems.BasePilotable;
@@ -27,6 +28,9 @@ import frc.robot.subsystems.Gobeur;
 import frc.robot.subsystems.Grimpeur;
 import frc.robot.subsystems.Lanceur;
 import frc.robot.subsystems.LimeLight;
+import frc.robot.commands.auto.Auto1Ballon;
+import frc.robot.commands.auto.Auto2Ballons;
+import frc.robot.commands.auto.Auto3Ballons;
 
 
 public class RobotContainer {
@@ -39,6 +43,14 @@ public class RobotContainer {
 
   XboxController pilote = new XboxController(0);
 
+  private final SendableChooser<Command> chooser = new SendableChooser<>();
+  //trajets
+  private final Command Auto1Ballon = new Auto1Ballon(basePilotable, gobeur, lanceur, limelight, convoyeur);
+  private final Command Auto2Ballons = new Auto2Ballons(basePilotable, gobeur, lanceur, limelight, convoyeur);
+  private final Command Auto3Ballons = new Auto3Ballons(basePilotable, gobeur, lanceur, limelight, convoyeur);
+  private final Command trajetVide = new WaitCommand(14);
+
+
   public class GrimpeurTrigger extends Trigger {
     @Override
     public boolean get() {
@@ -48,10 +60,18 @@ public class RobotContainer {
 
 
   public RobotContainer() {
-    
     configureButtonBindings();
+    chooser.setDefaultOption("Trajet Vide", trajetVide);
+    chooser.addOption("Trajet 1 Ballon", Auto1Ballon);
+    chooser.addOption("Tajet 2 Ballons", Auto2Ballons);
+    chooser.addOption("Trajet 3 Ballons", Auto3Ballons);
+
+    
+    SmartDashboard.putData(chooser);
+
     basePilotable.setDefaultCommand(new Conduire(pilote::getLeftY,pilote::getRightX, basePilotable));
     convoyeur.setDefaultCommand(new ConvoyerFancy(convoyeur));
+
 
   }
 
@@ -60,11 +80,15 @@ public class RobotContainer {
     //A = Gober
     new JoystickButton(pilote, Button.kA.value).toggleWhenPressed(new Gober(gobeur));
   
+    //B = Convoyer
+    new JoystickButton(pilote, Button.kA.value).toggleWhenPressed(new ConvoyerSimple(convoyeur, lanceur));
 
     //Y = Lancer en haut
     new JoystickButton(pilote, Button.kY.value).toggleWhenPressed(new ViserLancer(basePilotable, lanceur, convoyeur, limelight));//pas la bonne vitesse
 
-    
+    //X = LancerSimple
+    new JoystickButton(pilote, Button.kX.value).toggleWhenPressed(new LancerSimple(Constants.vitesseLancerBas, lanceur, convoyeur));
+
 
 
     //Trigger droit + joystick droit
@@ -74,21 +98,14 @@ public class RobotContainer {
 
     new JoystickButton(pilote, Button.kRightBumper.value).toggleWhenPressed(new AutoGrimper(20000, grimpeur) );//trouver la vraie hauteur
     new JoystickButton(pilote, Button.kLeftBumper.value).toggleWhenPressed(new AutoGrimper(0, grimpeur) );
-
-    //X = LancerSimple
-    new JoystickButton(pilote, Button.kX.value).toggleWhenPressed(new LancerSimple(Constants.vitesseLancerBas, lanceur, convoyeur));
-
   }
   
 
     public Command getAutonomousCommand() {
-
-     
-   //return new TrajetAuto("test", basePilotable);
-    //return new CaracteriserDrive(basePilotable);
-    //return new TournerLimelight(basePilotable, limelight);
-      return new CaracteriserLanceur(lanceur, convoyeur);
-    
-   
+      return chooser.getSelected().withTimeout(14.8)
+      //prepare Teleop
+      .andThen (new InstantCommand(() -> basePilotable.setBrake(false)))
+      .andThen (new InstantCommand(() -> basePilotable.setRamp(0.25)));
+      //return null;
   }
 }
--- src/main/java/frc/robot/commands/ConvoyerSimple.java
+++ src/main/java/frc/robot/commands/ConvoyerSimple.java
@@ -6,33 +6,37 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.Convoyeur;
+import frc.robot.subsystems.Lanceur;
 
-public class ConvoyerFancy extends CommandBase {
+public class ConvoyerSimple extends CommandBase {
   Convoyeur convoyeur;
-  public ConvoyerFancy(Convoyeur convoyeur) {
-    addRequirements(convoyeur);
+  Lanceur lanceur;
+  public ConvoyerSimple(Convoyeur convoyeur, Lanceur lanceur) {
     this.convoyeur = convoyeur;
+    this.lanceur = lanceur;
+    addRequirements(convoyeur);
+    addRequirements(lanceur);
   }
 
+  // Called when the command is initially scheduled.
   @Override
   public void initialize() {}
 
+  // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    if(convoyeur.capteurHaut() && !convoyeur.capteurBas()){
-      convoyeur.fournir();
-    }
-
-    else {
-      convoyeur.stop();
-    }
+    lanceur.setVitesseFeedForwardPID(1500);
+    convoyeur.fournir();
   }
 
+  // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
+    lanceur.stop();
     convoyeur.stop();
   }
 
+  // Returns true when the command should end.
   @Override
   public boolean isFinished() {
     return false;
